7d81de3dbe2a6c650b83040d7e2be727b32b2668
Alessandro Borione <leax_xiv@me.com>
2025-11-18 13:53:26 +0100

    Config project with Vuetify
    https://vuetifyjs.com/en/getting-started/installation/#existing-projects

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,30 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+// Vuetify
+// import "vuetify/styles"; // Probably an error on node_modules/vuetify/package.json, exports
+import "vuetify/styles/main.css";
+import { createVuetify } from "vuetify";
+import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import "unfonts.css";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+});
+
 const app = createApp(App);
 
 app.use(router);
+app.use(vuetify);
 
 app.mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,26 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
+import viteFonts from "unplugin-fonts/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/dm-tools/",
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    viteFonts({
+      fontsource: {
+        families: [
+          {
+            name: "Roboto",
+            weights: [100, 300, 400, 500, 700, 900],
+            styles: ["normal", "italic"],
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
