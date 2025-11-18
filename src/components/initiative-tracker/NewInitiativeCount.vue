321b20ab77a38069b8ba25acde43eb7644f0e26f
Alessandro Borione <leax_xiv@me.com>
2025-11-18 21:31:47 +0100

    Convert NewInitiativeCount with vuetify

--- src/components/initiative-tracker/NewInitiativeCount.vue
+++ src/components/initiative-tracker/NewInitiativeCount.vue
@@ -1,23 +1,40 @@
 <script setup lang="ts">
-import { ref, watch } from "vue";
+import type { ValidationRule } from "vuetify";
+import type { VForm } from "vuetify/components";
+
+import { ref, shallowRef, watch } from "vue";
 import InitiativeCount from "@model/InitiativeCount";
 import { mdiAvTimer } from "@mdi/js";
+
 const emit = defineEmits<{
   (e: "new-initiative-count", initiativeCount: InitiativeCount): void;
 }>();
 
-const open = ref(false);
+const open = shallowRef(false);
+const form = ref<VForm>();
 
-const name = ref("");
-const initiative = ref("");
+const name = ref<string>();
+const initiative = ref<number>(20);
+const initiativeRules = ref<ValidationRule[]>([
+  (v: number) => v !== null || "Initiative is required",
+  (v: number) =>
+    (v && typeof v === "number" && v >= 0 && Number.isInteger(v)) ||
+    "Initiative must be a positive integer",
+]);
 
 watch(open, () => {
+  if (!form.value)
+    // Protection during component mounting
+    return;
+
   name.value = "";
-  initiative.value = "";
+  initiative.value = 20;
 });
 
 function save() {
-  emit("new-initiative-count", new InitiativeCount(Number(initiative.value), name.value));
+  if (!form.value.isValid) return;
+
+  emit("new-initiative-count", new InitiativeCount(initiative.value, name.value));
   open.value = false;
 }
 </script>
@@ -25,34 +42,37 @@ function save() {
 <template>
   <v-btn :prepend-icon="mdiAvTimer" block @click.stop.prevent="open = true">Iniziativa</v-btn>
 
-  <Teleport to="body">
-    <div v-if="open" class="modal">
-      <p>Hello from the modal!</p>
+  <v-dialog v-model="open" max-width="600" persistent>
+    <v-card title="Iniziativa">
+      <v-form validate-on="invalid-input" @submit.prevent="save" ref="form">
+        <v-card-text>
+          <v-row>
+            <v-col cols="12" md="5">
+              <v-number-input
+                label="Iniziativa"
+                control-variant="split"
+                :rules="initiativeRules"
+                v-model="initiative"
+                required
+              />
+            </v-col>
+            <v-col>
+              <v-text-field v-model="name" label="Nome" />
+            </v-col>
+          </v-row>
+        </v-card-text>
 
-      <form @submit.stop.prevent="save">
-        <div>
-          <input v-model.lazy="initiative" type="number" required placeholder="Iniziativa" />
-          <input v-model.lazy="name" placeholder="Nome" />
-        </div>
+        <v-divider></v-divider>
 
-        <button type="reset" @click="open = false">Annulla</button>
-        <button type="submit">Salva</button>
-      </form>
-    </div>
-  </Teleport>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn type="reset" text="Annulla" variant="plain" @click="open = false"></v-btn>
+          <v-btn type="submit" color="primary" text="Salva" variant="tonal"></v-btn>
+        </v-card-actions>
+      </v-form>
+    </v-card>
+  </v-dialog>
 </template>
 
-<style scoped>
-.modal {
-  position: fixed;
-  z-index: 999;
-  top: 20%;
-  left: 50%;
-  width: 300px;
-  margin-left: -150px;
-
-  padding: 0.5em;
-  border: 1px solid black;
-  background-color: white;
-}
-</style>
+<style scoped></style>
