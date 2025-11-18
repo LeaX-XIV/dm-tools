5f8fd2f390e7890f61a3684b512cfcabf0e71d7c
Alessandro Borione <leax_xiv@me.com>
2025-11-18 18:13:38 +0100

    Fix icons

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,8 @@ import { createVuetify } from "vuetify";
 import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
-import "unfonts.css";
+
+import "@mdi/font/css/materialdesignicons.css";
 
 const vuetify = createVuetify({
   components,
