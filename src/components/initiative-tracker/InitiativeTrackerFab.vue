74599442315cd9810efa66f2ffd1a8ea2079a42f
Alessandro Borione <leax_xiv@me.com>
2025-11-28 19:03:31 +0100

    Add InitiativeTracketFab component

---
+++ src/components/initiative-tracker/InitiativeTrackerFab.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useInitiativeTracker } from "@state/InitiativeTrackerState.ts";
+import { mdiUpdate } from "@mdi/js";
+
+const { canAdvanceInitiative, advanceInitiative } = useInitiativeTracker();
+</script>
+
+<template>
+  <v-fab
+    v-if="canAdvanceInitiative"
+    :active="canAdvanceInitiative"
+    location="bottom right"
+    :icon="mdiUpdate"
+    app
+    @click="advanceInitiative"
+  />
+</template>
+
+<style scoped></style>
